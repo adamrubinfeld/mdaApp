0e4ff26d687443cd67a15a96d97425eca0e32383
Adam Rubinfeld <adamrubin01@gmail.com>
2021-01-25 16:04:18 +0200

    dror

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,18 +1,19 @@
 import mongoose , { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
+    _id: Number,
     firstName: String, 
     lastName: String, 
-    _id: Number,
     password: Number,
     hours?: Number
 }
 
 const UserSchema = new Schema({
+    _id: {type: Number, required: true, unique: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    _id: {type: Number, required: true, unique: true},
-    password: {type: Number, required: true}
+    password: {type: Number, required: true},
+    hours: {type: Number}
 })
 
 export default mongoose.model<IUser>('User', UserSchema);
